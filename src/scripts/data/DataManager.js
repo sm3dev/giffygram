9ba7c01bbbbdddfdadeb9a9e5bcbfa43a064a82b
Michael Wright <info@saymoremultimedia.com>
2021-09-02 13:16:14 -0500

    fetch for posts and messages is working in console.log tests

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -1,6 +1,20 @@
 export const getUsers = () => {
+  return fetch("http://localhost:8088/users").then((response) =>
+    response.json()
+  );
+};
 
-    return fetch("http://localhost:8088/users")
-    .then(response => response.json())
-   
-}
+export const getPosts = () => {
+  return fetch("http://localhost:8088/posts")
+    .then((response) => response.json())
+    .then((parsedResponse) => {
+      // do something with response here
+      return parsedResponse;
+    });
+};
+
+export const getMessages = () => {
+  return fetch("http://localhost:8088/messages").then((response) =>
+    response.json()
+  );
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,15 @@
 import { getUsers } from "./data/DataManager.js";
+import { getPosts } from "./data/DataManager.js";
+import { getMessages } from "./data/DataManager.js";
 
-const allUsers = getUsers().then(apiUsers => {
-console.log("All the users are as follows: ", apiUsers)
-})
+const allUsers = getUsers().then((apiUsers) => {
+  console.log("All the users are as follows: ", apiUsers);
+});
 
-console.log("oh nowwwww", allUsers);
+const allPosts = getPosts().then((apiPosts) => {
+  console.log("All the posts are here now:", apiPosts);
+});
+
+const allMessages = getMessages().then((apiMessages) => {
+  console.log("Got all the messages now too!", apiMessages);
+});
