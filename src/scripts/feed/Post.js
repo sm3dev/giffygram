92272d8d4d2ec99acfeb18b7453874436d0f82dd
Michael Wright <info@saymoremultimedia.com>
2021-09-08 09:32:25 -0500

    added button line to Post.js; it displays in HTML and shows the post id in the button ID attribute

--- src/scripts/feed/Post.js
+++ src/scripts/feed/Post.js
@@ -1,12 +1,31 @@
+// import { getUsers, getPosts } from "../data/DataManager.js";
+
 export const Post = (postObject) => {
-    return `
-      <section class="post">
-        <header>
-            <h2 class="post__tiEntrytle">${postObject.title}</h2>
-        </header>
-        <img class="post__image" src="${postObject.imageURL}" />
-      </section>
-    `
+    const dateString = new Date(postObject.dateCreated);
+    const formattedDate = dateString.toDateString();
+    // const nameOfUser = (postObj) => {
+    //     // tell me the post author user id
+    //     const whichUser = postObj.authorId;
+    //     // const foundUser = userObj[whichUser + 1].name;
+        
+    //     // this function needs to return a users.name value
+    //     return foundUser;
+    // }
+    // nameOfUser(postObject, userObject);
+    return `<section class="post">
+    <header>
+        <h2 class="post__titleEntry">${postObject.title}</h2>
+    </header>
+    <img class="post__image" src="${postObject.imageURL}" />
+    <section class="post-description__block">
+        <p class="post-description__text">${postObject.description}</p></section>
+    <section class="author-dated-posted__block">
+        <section class="post__author"><span class="post-author__text">Create by: Name goes here</span></section>
+        <section class="post__created-date">Posted: ${formattedDate}</section>
+    </section>
+    <div><button id="edit--${postObject.id}">Edit</button></div>
+
+</section>`
   }
 
 //   reference: https://github.com/nss-day-cohort-51/client-side-mastery/blob/main/book-2-giffygram/chapters/47-GG-PostList-Post.md
